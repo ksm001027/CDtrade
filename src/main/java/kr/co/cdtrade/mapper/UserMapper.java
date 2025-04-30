59a024182037341c454740df94cd0d1ece19ab19
GYURAE\zmdjd <zmdjdjr1031@naver.com>
2025-04-30 17:25:34 +0900

    feat: 회원가입 화면(register-form.jsp) 구현 email-check, nickname-check, tel-check로 ajax get 요청을 보내서 중복체크 확인 register.jsp에서 Mybatis로 회원정보 DB에 저장 회원가입이 완료되면 회원가입 완료페이지(register-complete.jsp)로 넘어가고 login-form.jsp에서 로그인 가능 login.jsp에서 Mybatis로 회원정보 DB에 저장하고 logout.jsp에서 로그아웃 가능 nav.jsp에서 네비바 구현

---
+++ src/main/java/kr/co/cdtrade/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package kr.co.cdtrade.mapper;
+
+import kr.co.cdtrade.vo.User;
+
+public interface UserMapper {
+
+	void insertUser(User user);
+	
+	User getUserByEmail(String email);
+	
+	User getUserByNickname(String nickname);
+	
+	User getUserByTel(String tel);
+}
